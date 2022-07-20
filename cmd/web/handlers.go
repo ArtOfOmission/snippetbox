1d7670dbffcf1dd4f0a49ab8f8588c5825494714
cys_jpt <aichenyansen@icloud.com>
2022-07-20 19:54:12 +0800

    重新组织结构

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,57 +2,32 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
-
-	w.Write([]byte("Hello from SnippetBox"))
+	w.Write([]byte("Hello from Snippetbox"))
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
 	}
-
-	//w.Write([]byte("Display a new snippet..."))
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
-
 	if r.Method != http.MethodPost {
-
 		w.Header().Set("Allow", http.MethodPost)
-
-		// w.WriteHeader(405)
-		// w.Write([]byte("Method NOt Allowd"))
-		//或者
-		http.Error(w, "Method Not Allowd", 405)
+		http.Error(w, "Method Not Allowed", 405)
 		return
 	}
-
 	w.Write([]byte("Create a new snippet..."))
-}
-
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
-
-	log.Println("Starting server on :4000")
-
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
 }
